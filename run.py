71178663af1db427d7c8213709b9f90ecc495e10
scottkstewart <scottkstewart16@gmail.com>
2015-12-01 19:42:35 -0500

    General fixes

--- run.py
+++ run.py
@@ -8,14 +8,13 @@ bots = []
 count = 0
 for str in accounts.keys():
     bots.append(accounts[str])
-    bots[count].update()
-    bots[count].printGrades()
     
     count += 1
 
 while True:
-    time.sleep(stuff['interval'])
     for b in bots:
         b.check()
+    
+    time.sleep(stuff['interval'])
 
     d.sync()
