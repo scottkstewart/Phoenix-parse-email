df37b01668cb1b65bd3c36ca750acfe44d7808a6
Scott Stewart <scottkstewart16@gmail.com>
2016-07-25 21:11:23 -0400

    Combined accounts/stuff database

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import os
 from distutils.core import setup
 setup(name='PPE',
-      version='1.0',
+      version='1.1',
       description='Phoenix-Parse-Email',
       author='Scott Stewart',
       author_email='scottkstewart16@gmail.com',
-      requires=['beautifulsoup4', 'requests', 'lxml', 'daemonize', 'dbm-gnu']
+      requires=['beautifulsoup4', 'requests', 'lxml', 'daemonize', 'dbm'],
       py_modules=['ppeMod'])
 os.system("mkdir /etc/ppe; touch /etc/ppe/log; touch /etc/ppe/runlog; chmod 777 -Rf /etc/ppe; cp phoenix /usr/bin/phoenix")
+
